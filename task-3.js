09e057dd48a5387f7e6098e535b2886364cb26bc
aldoBangun <punk.aldo68@gmail.com>
2022-09-22 07:16:29 +0800

    add task-3

--- task-3.js
+++ task-3.js
@@ -1,26 +1,34 @@
-const expectedResult = [
-  {
-    session_name: 'first test',
-    classes: [
-      {
-        students: [
-          {
-            student_name: 'John',
-          },
-        ],
-      },
-    ],
-  },
-  {
-    classes: [
-      {
-        class_name: 'second class',
-        students: [
-          {
-            student_name: 'Doe',
-          },
-        ],
-      },
-    ],
-  },
-]
+const numbers = [9, 6, 4, 2, 3, 5, 7, 0, 1]
+
+function sortNumber(numbers) {
+  for(let i=numbers.length; i > 1; i--) {
+    for(let j=0; j < i; j++) {
+      let temp
+      
+      if (numbers[j] > numbers[j+1]) {
+        temp = numbers[j]
+        numbers[j] = numbers[j+1]
+        numbers[j+1] = temp
+      }
+    }
+  }
+  return numbers
+}
+
+function checkMissingNumbers(numbers) {
+  const sortedNumbers = sortNumber(numbers)
+
+  for(let i=0; i < sortedNumbers.length; i++) {
+    const lastNumber = sortedNumbers.length - 1
+
+    if (i !== lastNumber) {
+      if (sortedNumbers[i] + 1 !== sortedNumbers[i + 1]) {
+        return sortedNumbers[i] + 1
+      } 
+    }
+  }
+  return 'No missing number'
+}
+
+const missingNumber = checkMissingNumbers(numbers)
+console.log(missingNumber)
